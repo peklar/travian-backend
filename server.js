31d6a6cd1cc51c878fc602f137c51258bd54c657
Alen <alen.peklar7@gmail.com>
2025-11-16 15:20:20 +0100

    Update

--- server.js
+++ server.js
@@ -72,4 +72,7 @@ app.post("/save", (req, res) => {
 });
 
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
